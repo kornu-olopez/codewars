6d2d109926c17bbf4e466f522c9868290d2b24fe
kornu-olopez <olopez@kornu.mx>
2025-09-29 09:45:48 -0600

    typescript

---
+++ TypeScript/1.ts
@@ -0,0 +1,19 @@
+// Your task is to write a function which returns the time since midnight in milliseconds.
+
+// Example:
+// h = 0
+// m = 1
+// s = 1
+
+// result = 61000
+// Input constraints:
+
+// 0 <= h <= 23
+// 0 <= m <= 59
+// 0 <= s <= 59
+
+export function past(h: number, m: number, s: number): number {
+  return 0;
+}
+
+console.log(past(0, 1, 1)); 
