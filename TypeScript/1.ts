a7b088ef2bff1e57a0d8481335338517f4b8947d
kornu-olopez <olopez@kornu.mx>
2025-10-05 14:22:29 -0600

    4

--- TypeScript/1.ts
+++ TypeScript/1.ts
@@ -12,11 +12,11 @@
 // 0 <= m <= 59
 // 0 <= s <= 59
 
-// assert.equal(past(0, 1, 1), 61000);
-//     assert.equal(past(1, 1, 1), 3661000);
-//     assert.equal(past(0, 0, 0), 0);
-//     assert.equal(past(1, 0, 1), 3601000);
-//     assert.equal(past(1, 0, 0), 3600000);
+// (past(0, 1, 1), 61000);
+// (past(1, 1, 1), 3661000);
+// (past(0, 0, 0), 0);
+// (past(1, 0, 1), 3601000);
+// (past(1, 0, 0), 3600000);
 
 export function past(h: number, m: number, s: number): number {
   return 0;
